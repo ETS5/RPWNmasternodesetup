5686ea8a9b0701201a3f8c15c4a6a9119b950d19
Brandon <35798060+ETS5@users.noreply.github.com>
2018-08-21 17:11:06 -0500

    Update RPWN-setup.sh

--- RPWN-setup.sh
+++ RPWN-setup.sh
@@ -175,15 +175,15 @@ fi
 
 #Installing Daemon
 cd ~
-mkdir ~/RPWNmasternodesetup/RPWN.linux_x64.v0.12.5.2
-wget https://github.com/RespawnPay/RPWN/releases/download/v.0.12.5.2/RPWN.linux_x64.v0.12.5.2.tar.gz
-tar -xvf RPWN.linux_x64.v0.12.5.2.tar.gz -C ~/RPWNmasternodesetup/RPWN.linux_x64.v0.12.5.2
-rm -rf RPWN.linux_x64.v0.12.5.2.tar.gz
+mkdir ~/RPWNmasternodesetup/RPWN.linux_x64.v0.12.5.4
+wget https://github.com/RespawnPay/RPWN/releases/download/v.0.12.5.4/RPWN.linux_x64.v0.12.5.4.tar.gz
+tar -xvf RPWN.linux_x64.v0.12.5.4.tar.gz -C ~/RPWNmasternodesetup/RPWN.linux_x64.v0.12.5.4
+rm -rf RPWN.linux_x64.v0.12.5.4.tar.gz
 
 stop_daemon
 
 # Deploy binaries to /usr/bin
-sudo cp RPWNmasternodesetup/RPWN.linux_x64.v0.12.5.2/RPWN* /usr/bin/
+sudo cp RPWNmasternodesetup/RPWN.linux_x64.v0.12.5.4/RPWN* /usr/bin/
 sudo chmod 755 -R ~/RPWNmasternodesetup
 sudo chmod 755 /usr/bin/RPWN*
 
